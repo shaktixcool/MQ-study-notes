b6792ee6550380a308ee99afd85fad7df26f445d
wystan2015 <617020899@qq.com>
2016-10-23 13:23:16 -0400

    add file

--- defmq.cmd
+++ defmq.cmd
@@ -3,3 +3,13 @@ def ql (Q)
 alter ql(Q) MAXDEPTH(5)
 
 delete ql(Q)
+
+define channel (C) chltype (rcvr)
+
+define channel (cs) chltype (sdr) conname('127.0.0.1 (1414)') xmitq (xq)
+
+alter channel (cs) chltype (sdr) batchsz(50)
+
+display channel (c) hbint
+
+
