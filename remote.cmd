f9140f5b687d6ece0d69d356ce2b7f605997e096
wystan2015 <617020899@qq.com>
2016-10-23 13:25:23 -0400

    add file

---
+++ remote.cmd
@@ -0,0 +1,13 @@
+define	channel	(c_qm1.qm2)	+
+	chltype	(rcvr)	+
+	trptype	(tcp)	+
+	replace
+define	channel	(c_qm2.qm1)	+
+	chltype	(sdr)	+
+	trptype	(tcp)	+
+	conname	('192.168.189.129 (1415)')	+
+	xmitq	(qm1)	+
+	replace
+define	qlocal	(qm1)	+
+	usage	(xmitq)	+
+	replace
